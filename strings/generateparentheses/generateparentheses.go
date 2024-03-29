806492ae02598ec5ddfb2a484c7f6069acb40b2e
donnol <donnol@outlook.com>
2024-03-29 13:58:42 +0800

    chore: 回溯之括号生成

--- strings/generateparentheses/generateparentheses.go
+++ strings/generateparentheses/generateparentheses.go
@@ -7,13 +7,19 @@
 
 package generateparentheses
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func GenerateParenthesis(n int) []string {
+	i := 0
 	result := make([]string, 0)
 	maxLen := 2 * n
 	var recursiveComputation func(s []string, left int, right int)
 	recursiveComputation = func(s []string, left int, right int) {
+		i++
+		fmt.Println(i, s, left, right)
 		if len(s) == maxLen {
 			result = append(result, strings.Join(s, ""))
 			return
@@ -21,12 +27,16 @@ func GenerateParenthesis(n int) []string {
 		if left < n {
 			s = append(s, "(")
 			recursiveComputation(s, left+1, right)
-			s = s[:len(s)-1]
+			fmt.Println(i, "left before", s)
+			s = s[:len(s)-1] // 去掉最后一个元素
+			fmt.Println(i, "left after", s)
 		}
 		if right < left {
 			s = append(s, ")")
 			recursiveComputation(s, left, right+1)
+			fmt.Println(i, "right before", s)
 			_ = s[:len(s)-1]
+			fmt.Println(i, "right after", s)
 		}
 	}
 	recursiveComputation(make([]string, 0), 0, 0)
